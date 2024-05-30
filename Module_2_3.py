f4bce04e750d552eafcc6673a8b8a77f5e23dc65
Wartol <naruto62788@gmail.com>
2024-05-30 18:58:00 +0500

    Add files via upload

---
+++ Module_2_3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+list_number = 0
+while len(my_list) > list_number:
+    if my_list[0 + list_number] > 0:
+        print(my_list[0 + list_number])
+        list_number = list_number + 1
+    else:
+        list_number = list_number + 1
+list_number = 0
